11ae70f6babe28bd7ef9a91b61cc2c3eb03781c9
Jason Uithol <jasonuithol@gmail.com>
2025-10-25 12:24:16 +1000

    another python version check

--- UltimatePyve.cmd
+++ UltimatePyve.cmd
@@ -1,2 +1,12 @@
-pip install -r requirements.txt
-python main.py
+@echo off
+py -3.13 -m pip install -r requirements.txt
+if errorlevel 1 (
+    echo( 
+    echo -------------------------------------------
+    echo(
+    echo ====! Must have python 3.13 installed !=====    
+    echo(
+    pause
+    exit
+)
+py -3.13 main.py
